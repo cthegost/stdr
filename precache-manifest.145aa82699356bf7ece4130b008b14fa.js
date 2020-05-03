00af3c0381d7e232e35c1c730fdda8fd68fea76d
cthegost <cthegost@gmail.com>
2020-05-03 18:36:10 +0300

    Updates

---
+++ precache-manifest.145aa82699356bf7ece4130b008b14fa.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b3a7054d87ccc64de0508f16fc2686b",
+    "url": "/stdr/index.html"
+  },
+  {
+    "revision": "ecb2a5cfd477c4122d65",
+    "url": "/stdr/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "a52c5975848cdd241f17",
+    "url": "/stdr/static/css/main.feafd9ef.chunk.css"
+  },
+  {
+    "revision": "ecb2a5cfd477c4122d65",
+    "url": "/stdr/static/js/2.a1537951.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/stdr/static/js/2.a1537951.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a52c5975848cdd241f17",
+    "url": "/stdr/static/js/main.40a5e47a.chunk.js"
+  },
+  {
+    "revision": "4e90cca664b7c7c159d6",
+    "url": "/stdr/static/js/runtime-main.f4c0c905.js"
+  },
+  {
+    "revision": "1e2b3e4d3671acd704cd286e0c2c806a",
+    "url": "/stdr/static/media/SF Pro Display Light.1e2b3e4d.svg"
+  },
+  {
+    "revision": "4408d5d7952c30d53ff55c2348d5a859",
+    "url": "/stdr/static/media/SF Pro Display Light.4408d5d7.woff"
+  },
+  {
+    "revision": "a6a354f432e298dc6796eef9bdf4a067",
+    "url": "/stdr/static/media/SF Pro Display Light.a6a354f4.woff2"
+  },
+  {
+    "revision": "dcea1bdac1b647543100c608453ad34d",
+    "url": "/stdr/static/media/SF Pro Display Light.dcea1bda.eot"
+  },
+  {
+    "revision": "2219fb6fc6ffd47839a2d4c562745f06",
+    "url": "/stdr/static/media/SF Pro Display Medium.2219fb6f.woff2"
+  },
+  {
+    "revision": "bce942fa707b7c009b29db8b4c8e9545",
+    "url": "/stdr/static/media/SF Pro Display Medium.bce942fa.woff"
+  },
+  {
+    "revision": "f28235bdecb71b4ed9985eb0013d5c99",
+    "url": "/stdr/static/media/SF Pro Display Medium.f28235bd.svg"
+  },
+  {
+    "revision": "f3466f09aa76e7921c84f412333a058a",
+    "url": "/stdr/static/media/SF Pro Display Medium.f3466f09.eot"
+  },
+  {
+    "revision": "90a5fb2a3fc3cb7283fa8f7242ba4c2a",
+    "url": "/stdr/static/media/events.90a5fb2a.jpg"
+  },
+  {
+    "revision": "3a7635d4dd318b1797a7a4aa25e4647e",
+    "url": "/stdr/static/media/google-hq.3a7635d4.jpg"
+  },
+  {
+    "revision": "52732ef9f63e6a06f52a6387ae5c9890",
+    "url": "/stdr/static/media/marketing.52732ef9.jpeg"
+  },
+  {
+    "revision": "4a8d6212d6a70d99821df26d742d96c8",
+    "url": "/stdr/static/media/startup.4a8d6212.jpg"
+  },
+  {
+    "revision": "393b3eb9bf5b92d5ce33cdde16330b80",
+    "url": "/stdr/static/media/success.393b3eb9.jpg"
+  },
+  {
+    "revision": "2fc79cd18bf32ec82b60ed81f620a8eb",
+    "url": "/stdr/static/media/webdev.2fc79cd1.png"
+  },
+  {
+    "revision": "6fd1e5bffb2610652c31089cdec07820",
+    "url": "/stdr/static/media/withus.6fd1e5bf.jpg"
+  }
+]);
